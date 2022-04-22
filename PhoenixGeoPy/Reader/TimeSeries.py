ab6fcc53f27fdf0e78429352c21dd0fcda0865f6
JP <peacock.jared@gmail.com>
2022-04-21 18:01:04 -0700

    added header class

--- PhoenixGeoPy/Reader/TimeSeries.py
+++ PhoenixGeoPy/Reader/TimeSeries.py
@@ -18,6 +18,18 @@ from PhoenixGeoPy.Reader.DataScaling import DataScaling
 # =============================================================================
 class Header:
     def __init__(self, **kwargs):
+        self.report_hw_sat = False
+        self.header_size = 128
+        self.ad_plus_minus_range = 5.0  # differential voltage range that the A/D can measure (Board model dependent)
+        self.channel_type = "?"           # "E" or "H"
+        self.channel_main_gain = None     # The value of the main gain of the board
+        self.intrinsic_circuitry_gain = None  # Circuitry Gain not directly configurable by the user
+        self.total_circuitry_gain = None  # Total board Gain both intrinsic gain and user-seletable gain in circuit
+        self.total_selectable_gain = None  # Total of the gain that is selectable by the user (i.e. att * pre * gain)
+        self.lpf_Hz = None                   # Nominal cutoff freq of the configured LPF of the channel
+        self.preamp_gain = 1.0
+        self.attenuator_gain = 1.0
+        
         for key, value in kwargs.items():
             setattr(self, key, value)
             
@@ -34,11 +46,204 @@ class Header:
     def update(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
+            
+    def __populate_channel_type(self, config_fp):
+        if config_fp[1] & 0x08 == 0x08:
+            self.channel_type = "E"
+        else:
+            self.channel_type = "H"
+        # Channel type detected by electronics
+        # this normally matches self.channel_type, but used in electronics design and testing
+        if config_fp[1] & 0x20 == 0x20:
+            self.detected_channel_type = 'E'
+        else:
+            self.detected_channel_type = 'H'
+   
+    def __populate_lpf(self, config_fp):
+        if config_fp[0] & 0x80 == 0x80:            # LPF on
+            if config_fp[0] & 0x03 == 0x03:
+                self.lpf_Hz = 10
+            elif config_fp[0] & 0x03 == 0x02:
+                if (self.board_model_main == "BCM03" or self.board_model_main == "BCM06"):
+                    self.lpf_Hz = 1000
+                else:
+                    self.lpf_Hz = 100
+            elif config_fp[0] & 0x03 == 0x01:
+                if (self.board_model_main == "BCM03" or self.board_model_main == "BCM06"):
+                    self.lpf_Hz = 10000
+                else:
+                    self.lpf_Hz = 1000
+        else:                                      # LPF off
+            if (self.board_model_main == "BCM03" or self.board_model_main == "BCM06"):
+                self.lpf_Hz = 17800
+            else:
+                self.lpf_Hz = 10000
+   
+    def __popuate_peamp_gain(self, config_fp):
+        if self.channel_type == "?":
+            raise Exception("Channel type must be set before attemting to calculate preamp gain")
+        preamp_on = bool(config_fp[0] & 0x10)
+        self.preamp_gain = 1.0
+        if self.channel_type == "E":
+            if preamp_on is True:
+                if self.board_model_main == "BCM01" or self.board_model_main == "BCM03":
+                    self.preamp_gain = 4.0
+                    if (self.board_model_revision == "L"):
+                        #Account for BCM01-L experimental prototype
+                        self.preamp_gain = 8.0
+                else:
+                    self.preamp_gain = 8.0
+                    # Acount for experimental prototype BCM05-A
+                    if self.ch_hwv[0:7] == "BCM05-A":
+                        self.preamp_gain = 4.0
+    
+    def __populate_main_gain(self, config_fp):
+        # BCM05-B and BCM06 introduced different selectable gains
+        new_gains = True   # we asume any newer board will have the new gain banks
+        if self.board_model_main == "BCM01" or self.board_model_main == "BCM03":
+            # Original style 24 KSps boards and original 96 KSps boards
+            new_gains = False
+        if self.ch_hwv[0:7] == "BCM05-A":
+            # Acount for experimental prototype BCM05-A, which also had original gain banks
+            new_gains = False
+        if config_fp[0] & 0x0C == 0x00:
+            self.channel_main_gain = 1.0
+        elif config_fp[0] & 0x0C == 0x04:
+            self.channel_main_gain = 4.0
+        elif config_fp[0] & 0x0C == 0x08:
+            self.channel_main_gain = 6.0
+            if not new_gains:
+                self.channel_main_gain = 16.0
+        elif config_fp[0] & 0x0C == 0x0C:
+            self.channel_main_gain = 8.0
+            if not new_gains:
+                self.channel_main_gain = 32.0
+   
+    def __handle_sensor_range(self, config_fp):
+        """This function will adjust the intrinsic circuitry gain based on the
+           sensor range configuration in the configuration fingerprint
+           
+           For this, we consider that for the Electric channel, calibration path, or H-legacy
+           sensors all go through a 1/4 gain stage, and then they get a virtial x2 gain from
+           Single-ended-diff before the A/D. In the case of newer sensors (differential)
+           instead of a 1/4 gain stage, there is only a 1/2 gain stage
+           
+           Therefore, in the E,cal and legacy sensor case the circuitry gain is 1/2, while for
+           newer sensors it is 1
+           """
+        if self.channel_type == "?":
+            raise Exception("Channel type must be set before attemting to calculate preamp gain")
+        self.intrinsic_circuitry_gain = 0.5   
+        if self.channel_type == "H":
+            if config_fp[1] & 0x01 == 0x01:
+                self.intrinsic_circuitry_gain = 1.0
+   
+    def __populate_attenuator_gain(self, config_fp):
+        self.attenuator_gain = 1.0    # Asume attenuator off
+        if self.channel_type == "?":
+            raise Exception("Channel type must be set before attemting to calculate preamp gain")
+        attenuator_on = bool(config_fp[4] & 0x01)
+        if attenuator_on and self.channel_type == "E":
+            new_attenuator = True  # By default assume that we are dealing with a newer types of boards
+            if self.board_model_main == "BCM01" or self.board_model_main == "BCM03":
+                # Original style 24 KSps boards and original 96 KSps boards
+                new_attenuator = False
+            if self.cs_hwv[0:7] == "BCM05-A":
+                # Acount for experimental prototype BCM05-A, which also had original gain banks
+                new_attenuator = False
+   
+            if new_attenuator:
+                self.attenuator_gain = 523.0 / 5223.0
+            else:
+                self.attenuator_gain = 0.1
+   
+    def unpack_header(self, stream):
+        if self.header_size > 0:
+            # be sure to read from the beginning of the file
+            stream.seek(0)
+            header = stream.read(self.header_size)
+        else:
+            return
+        
+        header_info = {}      
+        self.file_type = unpack_from('B', header, 0)[0]
+        self.file_version = unpack_from('B', header, 1)[0]
+        self.length = unpack_from('H', header, 2)[0]
+        self.inst_type = unpack_from('8s', header, 4)[0].decode("utf-8").strip(' ').strip('\x00')
+        self.inst_serial = b''.join(unpack_from('cccccccc', header, 12)).strip(b'\x00')
+        self.rec_id = unpack_from('I', header, 20)[0]
+        self.ch_id = unpack_from('B', header, 24)[0]
+        self.file_sequence = unpack_from('I', header, 25)[0]
+        self.frag_period = unpack_from('H', header, 29)[0]
+        self.ch_hwv = unpack_from('8s', header, 31)[0].decode("utf-8").strip(' ')
+        self.board_model_main = self.ch_hwv[0:5]
+        self.board_model_revision = self.ch_hwv[6:1]
+        self.ch_ser = unpack_from('8s', header, 39)[0].decode("utf-8").strip('\x00')
+        # handle the case of backend < v0.14, which puts '--------' in ch_ser
+        if all(chars in string.hexdigits for chars in self.ch_ser):
+            self.ch_ser = int(self.ch_ser, 16)
+        else:
+            self.ch_ser = 0
+        self.ch_fir = hex(unpack_from('I', header, 47)[0]) 
+        self.conf_fp = unpack_from('BBBBBBBB', header, 51)
+        self.update(**header_info)
+        
+        # Channel type
+        self.__populate_channel_type(self.conf_fp)
+        # Electric channel Preamp
+        self.__popuate_peamp_gain(self.conf_fp)
+        # LPF
+        self.__populate_lpf(self.conf_fp)
+        # Main Gain Stage
+        self.__populate_main_gain(self.conf_fp)
+        # Sensor range
+        self.__handle_sensor_range(self.conf_fp)
+        # Electric channel attenuator
+        self.__populate_attenuator_gain(self.conf_fp)
+        # Board-wide gains
+        self.total_selectable_gain = self.channel_main_gain * self.preamp_gain * self.attenuator_gain
+        self.total_circuitry_gain = self.total_selectable_gain * self.intrinsic_circuitry_gain
+   
+        header_info = {}
+        self.sample_rate_base = unpack_from('H', header, 59)[0]
+        self.sample_rate_exp = unpack_from('b', header, 61)[0]
+        self.sample_rate = self.sample_rate_base
+        if self.sample_rate_exp != 0:
+            self.sample_rate *= pow(10, self.sample_rate_exp)
+        self.bytes_per_sample = unpack_from('B', header, 62)[0]
+        self.frame_size = unpack_from('I', header, 63)[0]
+        self.dataFooter = self.frame_size >> 24
+        self.frameSize = self.frame_size & 0x0ffffff
+        self.decimation_node_id = unpack_from('H', header, 67)[0]
+        self.frame_rollover_count = unpack_from('H', header, 69)[0]
+        self.gps_long = unpack_from('f', header, 71)[0]
+        self.gps_lat = unpack_from('f', header, 75)[0]
+        self.gps_height = unpack_from('f', header, 79)[0]
+        self.gps_hacc = unpack_from('I', header, 83)[0]
+        self.gps_vacc = unpack_from('I', header, 87)[0]
+        self.timing_status = unpack_from('BBH', header, 91)
+        self.timing_flags = self.timing_status[0]
+        self.timing_sat_count = self.timing_status[1]
+        self.timing_stability = self.timing_status[2]
+        self.future1 = unpack_from('b', header, 95)[0]
+        self.future2 = unpack_from('i', header, 97)[0]
+        self.saturated_frames = unpack_from('H', header, 101)[0]
+        if self.saturated_frames & 0x80 == 0x80:
+            self.saturated_frames &= 0x7F
+            self.saturated_frames <<= 4
+        self.missing_frames = unpack_from('H', header, 103)[0]
+        self.battery_voltage_mV = unpack_from('H', header, 105)[0]
+        self.min_signal = unpack_from('f', header, 107)[0]
+        self.max_signal = unpack_from('f', header, 111)[0]
+        
+        self.update(**header_info)
+    
 
 
 class _TSReaderBase(object):
     def __init__(self, path, num_files=1, header_size=128, report_hw_sat=False):
         self._seq = None
+        
         self.base_path = path
         self.last_seq = self.seq + num_files
         self.stream = None
@@ -105,44 +310,39 @@ class _TSReaderBase(object):
     @property
     def max_samples(self):
         return int((self.file_size - self.header_size * 3) / 3)
+    @property
+    def sequence_list(self):
+        """
+        get all the files in the sequence
+        """
+        return sorted(list(self.base_dir.glob(f"*{self.file_extension}")))
+    
+    def _open_file(self, filename):
+        if filename.exists():
+            print(f"Next file is {filename}... opening")
+            self.stream = open(filename, 'rb')
+            if self.header_size > 0:
+                self.dataHeader = self.stream.read(self.header_size)
+            return True
+        return False
     
     def open_next(self):
-        ret_val = False
         if self.stream is not None:
             self.stream.close()
         self.seq += 1
+        self.open_file_seq(self.seq)
         if self.seq < self.last_seq:
-            new_seq_str = "%08X" % (self.seq)
-            new_path = self.base_dir.joinpath(
-                f"{self.inst_id}_{self.rec_id}_{self.ch_id}_{new_seq_str}{self.file_extension}"
-                )
+            new_path = self.sequence_list[self.seq - 1]
+            return self._open_file(new_path)
+        return False
 
-            if new_path.exists():
-                print(f"Next file is {new_path}... opening")
-                self.stream = open(new_path, 'rb')
-                if self.header_size > 0:
-                    self.dataHeader = self.stream.read(self.header_size)
-                ret_val = True
-
-        return ret_val
-
-    def open_file_seq(self, file_seq_num):
-        ret_val = False
+    def open_file_seq(self, file_seq_num=None):
         if self.stream is not None:
             self.stream.close()
-        self.seq = file_seq_num
-        new_seq_str = "%08X" % (self.seq)
-        new_path = self.base_dir.joinpath(
-            f"{self.inst_id}_{self.rec_id}_{self.ch_id}_{new_seq_str}{self.file_extension}"
-            )
-        if new_path.exists():
-            print (f"--> opening {new_path}")
-            self.stream = open(new_path, 'rb')
-            if self.header_size > 0:
-                self.dataHeader = self.stream.read(self.header_size)
-            ret_val = True
-
-        return ret_val
+        if file_seq_num is not None:    
+            self.seq = file_seq_num
+        new_path = self.sequence_list[self.seq - 1]
+        return self._open_file(new_path)
 
     def __populate_channel_type(self, config_fp):
         if config_fp[1] & 0x08 == 0x08:
@@ -191,7 +391,7 @@ class _TSReaderBase(object):
                 else:
                     self.preamp_gain = 8.0
                     # Acount for experimental prototype BCM05-A
-                    if self.header.ch_hwv[0:7] == "BCM05-A":
+                    if self.ch_hwv[0:7] == "BCM05-A":
                         self.preamp_gain = 4.0
     
     def __populate_main_gain(self, config_fp):
@@ -200,7 +400,7 @@ class _TSReaderBase(object):
         if self.board_model_main == "BCM01" or self.board_model_main == "BCM03":
             # Original style 24 KSps boards and original 96 KSps boards
             new_gains = False
-        if self.header.ch_hwv[0:7] == "BCM05-A":
+        if self.ch_hwv[0:7] == "BCM05-A":
             # Acount for experimental prototype BCM05-A, which also had original gain banks
             new_gains = False
         if config_fp[0] & 0x0C == 0x00:
@@ -245,7 +445,7 @@ class _TSReaderBase(object):
             if self.board_model_main == "BCM01" or self.board_model_main == "BCM03":
                 # Original style 24 KSps boards and original 96 KSps boards
                 new_attenuator = False
-            if self.header.cs_hwv[0:7] == "BCM05-A":
+            if self.cs_hwv[0:7] == "BCM05-A":
                 # Acount for experimental prototype BCM05-A, which also had original gain banks
                 new_attenuator = False
 
@@ -430,11 +630,13 @@ class NativeReader(_TSReaderBase):
 
             dataFrame = self.stream.read(self.frameSize)
             if not dataFrame:
-                if not self.open_next():
-                    break
-                dataFrame = self.stream.read(self.frameSize)
-                if not dataFrame:
-                    break
+                break
+                # if not self.open_next():
+                #     break
+                
+                # dataFrame = self.stream.read(self.frameSize)
+                # if not dataFrame:
+                #     break
 
             dataFooter = unpack_from("I", dataFrame, self.frameSize - 4)
 
